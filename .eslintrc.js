f3ea7c7734f448bf9ae9d466056c737cf1c2559e
june6723 <june6723@naver.com>
2023-03-21 11:43:42 +0900

    :construction: parte 코드 복붙 전

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-acme`
-  extends: ["acme"],
+  // This tells ESLint to load the config from the package `eslint-config-parte`
+  extends: ["parte"],
   settings: {
     next: {
       rootDir: ["apps/*/"],
