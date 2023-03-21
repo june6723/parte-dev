3ad043555e362a44816e457b821928cbe32ac871
june6723 <june6723@naver.com>
2023-03-21 19:00:37 +0900

    :sparkles: 폴더명 변경

--- packages/ui/src/IconButton/IconButton.types.ts
+++
@@ -1,9 +0,0 @@
-import React, { HTMLAttributes } from "react";
-import { IconProps } from "../../../icons/src";
-
-export type IconButtonProps = HTMLAttributes<HTMLButtonElement> & {
-  variant: "primary" | "secondary" | "minimal" | "error";
-  Icon: React.ReactElement<IconProps>;
-  size?: number;
-  disabled?: boolean;
-};
